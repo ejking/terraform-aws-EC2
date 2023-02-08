28ef18fc9d383b5d6b844aa1bef46aec261dcefe
jmueller-hf <122904141+jmueller-hf@users.noreply.github.com>
2023-02-08 11:57:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,15 +9,15 @@ terraform {
 
 
 resource "aws_instance" "instance" {
-  ami                    = var.input_vars.ami
-  instance_type          = var.input_vars.instance_type
-  subnet_id              = var.input_vars.subnet_ids
-  key_name               = var.input_vars.key_name
-  user_data              = var.input_vars.user_data
-  iam_instance_profile   = var.input_vars.instance_profile
-  vpc_security_group_ids = var.input_vars.security_groups
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_ids
+  key_name               = var.key_name
+  user_data              = var.user_data
+  iam_instance_profile   = var.instance_profile
+  vpc_security_group_ids = var.security_groups
   tags = {
-    "Name"            = var.input_vars.instance_name
+    "Name"            = var.instance_name
     "Service Role"    = "EC2 Instance"
   }
 }
