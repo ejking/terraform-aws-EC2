f0733f3677b5a8919a5ce61988e09bd35103381b
ejking <eric@kinghometech.com>
2023-02-09 15:05:02 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "instance_id" {
   value       = aws_instance.instance.id
   description = "The instance id"
 }
+
+output "instance_ip" {
+  value       = aws_instance.instance.private_ip
+  description = "The instance private IPv4 address"
+}
